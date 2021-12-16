55b7afb7e2be3f48c54dc0c0b8a1f7e5ca68675f
delfinadelorenzo <delfinadelorenzo@gmail.com>
2021-12-16 15:30:39 -0300

    nav nuevo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,21 @@ import { Navbar, Container, Nav , NavDropdown } from 'react-bootstrap'
 export default function NavBar() {
     return (
         <div>
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="light" expand="lg" >
   <Container>
-    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Brand href="#home">BRAVE BIKINIS</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Inicio</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        <NavDropdown title="Shop" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Bikinis</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Enterizas</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Summer Sale!</NavDropdown.Item>
         </NavDropdown>
+        <Nav.Link href="#link">Como comprar</Nav.Link>
+
+
+
       </Nav>
     </Navbar.Collapse>
   </Container>
