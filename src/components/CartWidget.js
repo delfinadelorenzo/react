0ba97f5f498942844b020d74ce22e7f6864c8392
delfinadelorenzo <delfinadelorenzo@gmail.com>
2021-12-16 20:28:49 -0300

    carrito

---
+++ src/components/CartWidget.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { IoCartOutline } from 'react-icons/io5';
+
+
+const CartWidget = () => {
+    
+        return (
+            <IoCartOutline />
+        )
+    
+}
+
+export default CartWidget
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,29 @@
 import React from 'react'
-import { Navbar, Container, Nav , NavDropdown } from 'react-bootstrap'
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap'
+import CartWidget from './CartWidget'
 
 export default function NavBar() {
-    return (
-        <div>
-            <Navbar bg="light" expand="lg" >
-  <Container>
-    <Navbar.Brand href="#home">B R A V E  Swimwear</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <NavDropdown title="Shop" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Bikinis</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Enterizas</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Summer Sale!</NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#link">Como comprar</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
-        </div>
-    )
+  return (
+    <div>
+      <Navbar bg="light" expand="lg" >
+        <Container >
+          <Navbar.Brand href="#home">B R A V E  Swimwear</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown title="Shop" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Bikinis</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Enterizas</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Summer Sale!</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#link">Como comprar</Nav.Link>
+              <Container fluid>
+              <CartWidget />
+              </Container>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  )
 }
