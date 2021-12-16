23f921222810f26554fe8a67a40ca24966de0871
delfinadelorenzo <delfinadelorenzo@gmail.com>
2021-12-16 13:35:24 -0300

    cambio en el nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ export default function NavBar() {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#home">Inicio</Nav.Link>
         <Nav.Link href="#link">Link</Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
