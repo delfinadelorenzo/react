7656fefa9059f900eca5f5913f88b0e1b5dd9d79
delfinadelorenzo <delfinadelorenzo@gmail.com>
2021-12-16 16:47:06 -0300

    nombre y fotos

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
         <div>
             <Navbar bg="light" expand="lg" >
   <Container>
-    <Navbar.Brand href="#home">BRAVE BIKINIS</Navbar.Brand>
+    <Navbar.Brand href="#home">B R A V E  Swimwear</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="me-auto">
