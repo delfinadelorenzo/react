67e772d413fd27c9e91d2966b602a80dbe19afdb
delfinadelorenzo <delfinadelorenzo@gmail.com>
2021-12-21 15:04:30 -0300

    contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,16 +1,28 @@
 import { useState } from "react";
 
-function ItemCount (){
-    const  [ count, SetCount] = useState(0)
 
-    const aumentar = () => { SetCount ( count +1 )}
-    const disminuit= () => {SetCount (count - 1)}
-    const reset= () => {SetCount (0)}
+function ItemCount() {
+    const [count, SetCount] = useState(0)
 
-    return(
-        <div>
+    const aumentar = () => { SetCount(count + 1) }
+    const disminuir = () => { SetCount(count - 1) }
+    const reset = () => { SetCount(0) }
+
+    return (
+        <div style={{textAlign: "center"}}>
             
-        </div>
+            <span style={{display: "flow-root", color:"cornflowerblue", fontSize:"20px"}} > {count} </span>
+            <div>
+
+            <button  onClick={aumentar}> + </button>
+            <button  onClick={disminuir}> - </button>
+            <button  onClick={reset}> Reset </button>
+
+            </div>
+
+
+        </div >
     )
 
 }
+export default ItemCount
