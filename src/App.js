741e8aa9ae4454032b5a8abe370655219752c5b4
delfinadelorenzo <delfinadelorenzo@gmail.com>
2022-01-12 15:29:26 -0300

    cartContext

--- src/App.js
+++ src/App.js
@@ -5,26 +5,29 @@ import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Carrito from './components/Carrito';
+import { CartContextProvider } from './components/CartContext';
 
 
 function App() {
 
 
   return (
-    <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<div>HOME</div>} />
-          <Route path="/category/:idCategory" element={<ItemListContainer  />} />
-          <Route path="/category/:idCategory" element={<ItemListContainer  />} />
-          <Route path="/category/:idCategory" element={<ItemListContainer  />} />
-          <Route path="/detalle/:id" element={<ItemDetailContainer />}/>
-          <Route path="/cart" element={<Carrito/>}/>
-          <Route path="/comoComprar" element={<div>Como Comprar</div>}/>
-        </Routes>
-      </div>
-    </Router>
+    <CartContextProvider>
+      <Router>
+        <div className="App">
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<div>HOME</div>} />
+            <Route path="/category/:idCategory" element={<ItemListContainer />} />
+            <Route path="/category/:idCategory" element={<ItemListContainer />} />
+            <Route path="/category/:idCategory" element={<ItemListContainer />} />
+            <Route path="/detalle/:id" element={<ItemDetailContainer />} />
+            <Route path="/cart" element={<Carrito />} />
+            <Route path="/comoComprar" element={<div>Como Comprar</div>} />
+          </Routes>
+        </div>
+      </Router>
+    </CartContextProvider>
 
 
   );
---
+++ src/components/CartContext.js
@@ -0,0 +1,99 @@
+import React, { useState, createContext, useContext } from "react";
+
+
+const CartContext = createContext();
+
+const useCart = () => useContext(CartContext);
+
+const initialState = {
+  addedItems: [],
+  totalPrice: 0,
+};
+
+const CartContextProvider = ({ children }) => {
+  const [cart, setCart] = useState(initialState);
+
+
+  const addItem = (item, cantidad) => {
+    let itemToAdd = Object.assign(item, { cantidad: cantidad });
+
+    // actualizar su cantidad.
+    if (isInCart(itemToAdd.id)) {
+      const updateItemsAdded = cart.addedItems.map((producto) => {
+        if (producto.id === itemToAdd.id)
+          return {
+            ...producto,
+            cantidad:
+              producto.cantidad < producto.stock
+                ? producto.cantidad + cantidad
+                : producto.stock,
+          };
+        return producto;
+      });
+      return setCart({
+        addedItems: updateItemsAdded,
+        totalPrice: calculoTotalPrice(itemToAdd, cantidad),
+      });
+    }
+
+    setCart({
+      addedItems: [...cart.addedItems, itemToAdd],
+      totalPrice: calculoTotalPrice(itemToAdd, cantidad),
+    });
+  };
+
+  const calculoTotalPrice = (item, cantidad) => {
+    return cart.totalPrice + item.price * cantidad;
+  };
+
+  // ---------> VERIFICA SI UN ITEM ESTÁ EN EL CART <---------
+  const isInCart = (id) => {
+    return cart.addedItems.some((addedItem) => addedItem.id === id)
+      ? true
+      : false;
+  };
+
+  // ---------> REMOVER ITEM <---------
+  const removeItem = (id) => {
+    let updateTotalPrice = 0;
+    const updateItemsAdded = cart.addedItems.filter((producto) => {
+      if (producto.id !== id) {
+        // Actualizo el precio total ya que se está eliminando
+        // un elemento con su cantidad seleccionada.
+        updateTotalPrice += decrementTotalPrice(producto);
+      }
+      return producto.id !== id;
+    });
+    setCart({
+      totalPrice: updateTotalPrice,
+      addedItems: updateItemsAdded,
+    });
+  };
+
+  const decrementTotalPrice = (item) => {
+    let updateTotalPrice = 0;   
+      return (updateTotalPrice += item.price * item.cantidad);
+
+  };
+
+
+  const clear = () => {
+    setCart(initialState);
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        cart,
+        setCart,
+        addItem,
+        removeItem,
+        clear,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartContextProvider, useCart };
