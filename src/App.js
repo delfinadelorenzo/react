88fe244e4518be87e642df4bababacae9a036666
delfinadelorenzo <delfinadelorenzo@gmail.com>
2022-01-20 13:51:12 -0300

    firestore

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Carrito from './components/Carrito';
 import { CartContextProvider } from './components/CartContext';
 import "./components/App.scss"
+import { data } from "./components/data";
+import db from './firebase/Firebase';
+import { collection, addDoc } from 'firebase/firestore';
+import { fileUpload } from './firebase/Fileupload'
+
 
 function App() {
+  
+  const arrayUpload = () =>{
+    data.forEach (async (element)=>{
+      const imgURL = await fileUpload (element.pictureUrl)
+      addDoc(collection(db, 'products'), {...element, pictureUrl: imgURL})
+    })     
+    
+  }
+
+
 
 
   return (
@@ -16,6 +31,7 @@ function App() {
       <Router>
         <div className="App">
           <NavBar />
+          <button onClick={arrayUpload}>subir cosas</button>
           <Routes>
             <Route path="/" element={<div>HOME</div>} />
             <Route path="/category/:idCategory" element={<ItemListContainer />} />
